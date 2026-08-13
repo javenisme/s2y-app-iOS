4fd56767bc2ba75cf65c42708e7e8b331c3ec467
Javen <javenisme@gmail.com>
2026-08-12 23:57:31 -0400

    HLT-099: remove embedded test account credentials

--- S2Y/Firestore/FirebaseConfiguration.swift
+++ S2Y/Firestore/FirebaseConfiguration.swift
@@ -11,7 +11,6 @@ import FirebaseAuth
 import FirebaseStorage
 import Spezi
 import SpeziAccount
-import SpeziFirebaseAccount
 
 
 final class FirebaseConfiguration: Module, DefaultInitializable, @unchecked Sendable {
@@ -24,10 +23,7 @@ final class FirebaseConfiguration: Module, DefaultInitializable, @unchecked Send
     }
     
     
-    @Application(\.logger) private var logger
-    
     @Dependency(Account.self) private var account: Account? // optional, as Firebase might be disabled
-    @Dependency(FirebaseAccountService.self) private var accountService: FirebaseAccountService?
     
     
     @MainActor var userDocumentReference: DocumentReference {
@@ -62,39 +58,4 @@ final class FirebaseConfiguration: Module, DefaultInitializable, @unchecked Send
         Self.userCollection.document(accountId)
     }
     
-    func configure() {
-        Task {
-            await setupTestAccount()
-        }
-    }
-    
-    private func setupTestAccount() async {
-        guard let accountService, FeatureFlags.setupTestAccount else {
-            return
-        }
-        
-        do {
-            try await accountService.login(userId: "lelandstanford@stanford.edu", password: "StanfordRocks!")
-            return
-        } catch {
-            guard let accountError = error as? FirebaseAccountError,
-                  case .invalidCredentials = accountError else {
-                logger.error("Failed to login into test account: \(error)")
-                return
-            }
-        }
-        
-        // account doesn't exist yet, signup
-        var details = AccountDetails()
-        details.userId = "lelandstanford@stanford.edu"
-        details.password = "StanfordRocks!"
-        details.name = PersonNameComponents(givenName: "Leland", familyName: "Stanford")
-        details.genderIdentity = .male
-        
-        do {
-            try await accountService.signUp(with: details)
-        } catch {
-            logger.error("Failed to setup test account: \(error)")
-        }
-    }
 }
